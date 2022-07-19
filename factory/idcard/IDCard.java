e8bcc1e6ae205dc153b949ad48782abf89cae4cb
skanae <kanae11277@gmail.com>
2022-07-19 23:08:58 +0900

    factory

---
+++ factory/idcard/IDCard.java
@@ -0,0 +1,28 @@
+package idcard;
+
+import framework.Product;
+
+public class IDCard extends Product{
+    private String owner;
+    
+    IDCard(String owner){
+        System.out.println(owner + "のカードを作ります。");
+        this.owner = owner;
+    }
+
+    @Override
+    public void use(){
+        System.out.println(owner + "を使います");
+    }
+
+    @Override
+    public String toString(){
+        return "(" + owner + ")" ;
+    }
+
+    public String getOwner(){
+        return owner;
+    }
+
+
+}
